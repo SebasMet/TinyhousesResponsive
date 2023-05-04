b049762ce7147807abc44583c0ab2efa20d5a879
SebasMet <sebastian.metzger.nl@gmail.com>
2023-05-04 16:18:01 +0200

    Add Navbar and home image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import logo from './media/logo.png'
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="menubar">
+        <div id="logo">
+          <img id="logoimage" src={logo} alt="Tiny houses logo"></img>
+        </div>
+      </div >
+      <div id='imagecontainer'>
+
+      </div>
+    </>
   );
 }
 
