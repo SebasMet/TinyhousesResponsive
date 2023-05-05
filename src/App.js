8204d81114f14503645e93a5cdbeb7169d6d4c80
SebasMet <sebastian.metzger.nl@gmail.com>
2023-05-06 00:05:03 +0200

    Made Banner responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './media/logo.png'
+import tinyhouse from './media/tinyhouse1.jpg'
 import './App.css';
 import Banner from './components/Banner';
 
@@ -17,8 +18,17 @@ function App() {
       <div id='image-container'>
       </div>
 
-      <Banner>
-        
+      <Banner
+      head={"Titel"}
+      text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit"}
+      img={tinyhouse}
+      >
+      </Banner>
+      <Banner
+      head={"Titel"}
+      text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit"}
+      img={tinyhouse}
+      >
       </Banner>
       </>
   );
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,17 +2,16 @@ import "./Banner.css";
 
 function Banner({ head, text, img }) {
   return (
-    <div className="banner">    
-        <div className="text-section">
-            <p>{text}</p>
-        </div>
+    <div className="banner">
+      <div className="text-section">
+        <h1>{head}</h1>
+        <p>{text}</p>
+      </div>
 
-        <div className="img-section">
-            {img}
+      <div className="image-section">
+        <img id="image" src={img}></img>
 
-        </div>
-
-  
+      </div>
     </div>
   );
 }
